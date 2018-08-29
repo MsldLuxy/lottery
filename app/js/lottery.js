acd173efb671ff705a8d4352b926db5c99a9fff6
MsldLuxy <310870966@qq.com>
2018-08-29 13:15:23 +0800

    lottery start

--- app/js/lottery.js
+++ app/js/lottery.js
@@ -1,1 +1,6 @@
-
+import 'babel-polyfill';
+import Base from './lottery/base.js';
+import Calculate from './lottery/calculate.js';
+import Interface from './lottery/interface.js';
+import Timer from './lottery/timer.js';
+import $ from 'jquery';
